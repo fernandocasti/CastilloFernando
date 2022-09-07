964072aaefaad080652b95d4d51e77b84095771b
Fernando Castillo <castillo.fd@hotmail.com>
2022-09-06 22:05:08 -0300

    Castillo

---
+++ JS/carrito.js
@@ -0,0 +1,37 @@
+const pedirproducto = async () =>{
+    const mostrar = document.getElementById("catalogoVinos");
+    const resp = await fetch("https://api.mercadolibre.com/categories/MLA1404/attributes#json");
+    const data = await resp.json();
+    data.map( (lista) => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+            <div class="row">
+                <img src="https://loremflickr.com/100/100/${lista.name}">
+                <h4>${lista.name}</h4>
+                <p>${lista.id}</p>
+            </div>
+        `
+        mostrar.append(li)
+    });
+};
+pedirproducto();
+
+/* Metodo async creado para mostrar el id y el nombre del producto
+
+const pedirPost = async () => {
+    const posts = document.getElementById("catalogoVinos");
+
+    fetch(`https://api.mercadolibre.com/sites/MLA/search?category=MLA1404#json`)
+
+    .then( (response) => response.json())
+    .then( (data) => {
+        data.forEach( (post) => {
+            const li = document.createElement(`li`);
+            li.innerHTML = `
+            <h2>${post.results}</h2>
+            `;
+            posts.append(li);
+        });
+    });
+}
+pedirPost();*/
